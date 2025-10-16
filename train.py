14de39f63131c18860127dc15d4b886eb2e392cf
xuuuan <129193898+duckduckxuan@users.noreply.github.com>
2025-10-16 09:11:42 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -3,11 +3,16 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data="garbage.yaml",
-    epochs=50,
+    data="datasets/taco_yolo_fr/data.yaml",
+    epochs=100,
     imgsz=640,
+    batch=8,
+    workers=2,
+    device="cpu",
+    lr0=0.001,
+    val=True,
+    cache=True,
+    exist_ok=True,
     project="runs/detect",
-    name="best",
-    device='cpu'
-    # device=0
+    name="best"
 )
